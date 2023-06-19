f95d665a4203ac9c6956cee25e383e899747db63
Mohamad-L <136597544+Mohamad-L@users.noreply.github.com>
2023-06-19 02:10:38 -0400

    Add files via upload

---
+++ data/CommunitysofFocus_30.js
@@ -0,0 +1,8 @@
+var json_CommunitysofFocus_30 = {
+"type": "FeatureCollection",
+"name": "CommunitysofFocus_30",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Youth": "Mass 4H Foundation, Justice Resource Institute", "PplColor": "Authentic Carribean Foundation, Boston Chinatown Neighborhood Center, Center for Arabic Culture, Latino Public Institute, Islamic Society of Boston, Somali Parents Advocacy Center for Education", "ImmPplCol": "Cambobian Mutual Assistance Association", "OldPpl": "Central Massachusetts Agency on Aging", "Disability": "DEAF Inc, Disability Policy Consortium, Life Path", "SpecialCar": "Enlace de Familias, Family TIES of MA, Federation of Children with Special Needs", "Immigrants": "Everett Haitian Community Center, Immigrants Assistance Center Inc., QARI, VietAID, Volunteers in Medicine", "Pregnant": "Family Services of Merrimack Valley, Outer Cape Health Services", "Unhoused": "Father Bill's & MainSpring, Tapestry Health", "NoEnglish": "La Colaborativa, Massachusetts Alliance of Portuguese Speakers, New American Association of Massachusetts", "LGBTQ": "LGBT Asylum Task Force, Multicultural AIDs Coalition", "Incarcerat": "Transformational Prison Project" }, "geometry": { "type": "MultiPolygon", "coordinates": [ [ [ [ -70.619483474012071, 42.458699895138842 ], [ -70.262310156612457, 42.450516262229023 ], [ -70.260877097779669, 42.354948797874421 ], [ -70.614173214823623, 42.365562152138097 ], [ -70.619483474012071, 42.458699895138842 ] ] ] ] } }
+]
+}
